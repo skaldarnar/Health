93a9aec655d64f00d4a84774fd2e36ee13ac0ec1
E Aakash <09e.aakash@gmail.com>
2019-05-17 23:16:44 +0530

    Add javadoc for HealthComponent

--- src/main/java/org/terasology/HealthComponent.java
+++ src/main/java/org/terasology/HealthComponent.java
@@ -19,29 +19,46 @@ import org.terasology.entitySystem.Component;
 import org.terasology.network.Replicate;
 import org.terasology.rendering.nui.properties.TextField;
 
+/**
+ * Provides Health to entity attached with HealthComponent. Contains the parameters
+ * required for all health related events.
+ */
 public class HealthComponent implements Component {
 
+    /** Maximum allowed health, capped to this if exceeding this value. */
     @Replicate
     public int maxHealth = 20;
+
+    /** Amount of health restored in each regeneration tick. */
     @Replicate
     public float regenRate;
+
+    /** Time delay before regeneration starts. */
     @Replicate
     public float waitBeforeRegen;
 
+    /** Falling speed threshold above which damage is inflicted to entity. */
     @Replicate
     public float fallingDamageSpeedThreshold = 20;
+
+    /** Horizontal speed threshold above which damage is inflicted to entity. */
     @Replicate
     public float horizontalDamageSpeedThreshold = 20;
+
+    /** The multiplier used to calculate damage when horizontal or vertical threshold is crossed. */
     @Replicate
     public float excessSpeedDamageMultiplier = 10f;
 
+
+    /** The current value of health. */
     @Replicate
     @TextField
     public int currentHealth = 20;
 
-    // Regen info
+    /** Next tick time that will trigger regeneration. */
     public long nextRegenTick;
 
+    /** Used to send Destroy event when health breaches zero. */
     public boolean destroyEntityOnNoHealth;
 
 }
